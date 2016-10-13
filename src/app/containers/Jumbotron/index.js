9db622228ae6f26667ee87eccb5415105eda8165
mrsteele <matt@omnionline.us>
2016-10-12 22:33:29 -0400

    closes #9

--- src/app/containers/Jumbotron/index.js
+++ src/app/containers/Jumbotron/index.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react'
+import Slider from 'react-slick'
 
 export default class Jumptron extends Component {
+  getRandomImg (offset = 0) {
+    const t = Date.now() + offset
+    return `https://unsplash.it/1200/500?random&c=${t}`
+  }
+
   render () {
     return (
       <section className='jumbotron'>
         <div className='container text-right'>
-          <h1>Welcome</h1>
-          <p>This is the sample site.</p>
-          <p><a className='btn' tabIndex='0'>Link</a></p>
-          <p><button className='btn' tabIndex='0'>Button</button></p>
+          <Slider>
+            <div>
+              <h2 className='title text'>This is a test</h2>
+              <p className='description text'>This is the description of what will be here</p>
+              <img src={this.getRandomImg()} />
+            </div>
+            <div>
+              <h2 className='title text'>This is a test</h2>
+              <p className='description text'>This is the description of what will be here</p>
+              <img src={this.getRandomImg(1)} />
+            </div>
+          </Slider>
         </div>
       </section>
     )
